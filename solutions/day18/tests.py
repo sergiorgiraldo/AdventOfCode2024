4c71c3b56ecd1976691dbbd1c0cbdac26de51fae
Sergio Giraldo <sergio@giraldo.com.br>
2024-12-20 20:51:43 +0100

    chore: script to test all days ::by sergio giraldo @ 20241220T2051CET, gpg signed

--- solutions/day18/tests.py
+++ solutions/day18/tests.py
@@ -34,15 +34,38 @@ class Tests(unittest.TestCase):
             "1,6",
             "2,0"
         ]
-        start, end, fallen_bytes = solution.setup(input, True)
+        start, end, fallen_bytes, _ = solution.setup(input, True)
+        self.assertEqual(solution.FindShortestPath2Exit(start, end, fallen_bytes), 22, "Oops")
 
-        res = solution.FindShortestPath2Exit(start, end, fallen_bytes)
-
-        self.assertEqual(res, 22, "Oops")
-
-    # def test_part2(self):
-        # input = ""
-        # self.assertEqual(solution.pt2(input), "", "Oops")
+    def test_part2(self):
+        input = [
+                    "5,4",
+                    "4,2",
+                    "4,5",
+                    "3,0",
+                    "2,1",
+                    "6,3",
+                    "2,4",
+                    "1,5",
+                    "0,6",
+                    "3,3",
+                    "2,6",
+                    "5,1",
+                    "1,2",
+                    "5,5",
+                    "2,5",
+                    "6,5",
+                    "1,4",
+                    "0,4",
+                    "6,4",
+                    "1,1",
+                    "6,1",
+                    "1,0",
+                    "0,5",
+                    "1,6",
+                    "2,0"
+                ]        
+        self.assertEqual(solution.GetBadByte(input, True), "6,1", "Oops")
 
     def test_sanity_check(self):
         self.assertEqual(1 + 1, 2, "Oops")
